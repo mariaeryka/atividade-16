1d06dcd84498c3cb4ddaf1d2eedbc6176482abbe
mariaeryka <erykamariab4@gmail.com>
2024-09-11 15:50:00 -0300

    massa corporal

---
+++ att 16.py
@@ -0,0 +1,22 @@
+# Crie um programa que receba o peso e a altura de uma pessoa e calcule o Índice de Massa Corporal (IMC). O programa deve classificar o IMC da pessoa de acordo com a tabela a seguir:
+# Abaixo do peso: IMC < 18.5
+# Peso normal: 18.5 ≤ IMC < 25
+# Sobrepeso: 25 ≤ IMC < 30
+# Obesidade: IMC ≥ 30
+
+peso = float(input("Digite o peso (kg): "))
+altura = float(input("Digite altura (m): "))
+
+imc = peso / (altura ** 2)
+
+if imc < 18.5:
+    classificacao = "Abaixo do peso"
+elif imc < 25:
+    classificacao = "Peso normal"
+elif imc < 30:
+    classificacao = "Sobrepeso"
+else:
+    Classificacao = "Obesidade"
+
+print("IMC:", roud (imc, 2))
+print("Classificacao:", classificacao)
